afa3695e072e0b78be10ee6a0fa892dfade920e0
N-Yash <113545575+N-Yash@users.noreply.github.com>
2025-02-28 18:21:22 -0500

    Change routing

--- frontend/src/routes/app.routes.jsx
+++ frontend/src/routes/app.routes.jsx
@@ -6,6 +6,10 @@ import {
 import App from "../App";
 import { lazy } from "react";
 import ErrorPage from "../core/ErrorPage";
+import SignUp from "../pages/SignUp";
+import SignIn from "../pages/SignIn";
+import PassReset from "../pages/PassReset";
+import PassResetDone from "../pages/PassResetDone";
 
 const Dashboard = lazy(() => import("../pages/dashboard/Dashboard"));
 const UrlShortner = lazy(() => import("../pages/url-shortner/UrlShortner"));
@@ -13,6 +17,10 @@ const UrlShortner = lazy(() => import("../pages/url-shortner/UrlShortner"));
 const appRoutes = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/password-reset" element={<PassReset />} />
+      <Route path="/password-reset-done" element={<PassResetDone />} />
       <Route index element={<Dashboard />} /> {/* Default route */}
       <Route path="url-shortner" element={<UrlShortner />} />
     </Route>
