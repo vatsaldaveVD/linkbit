2e5a9de52a370342260a649d952bec287ea64f51
Kiran Gandhi <kiranrgandhi97@gmail.com>
2025-03-01 10:01:44 -0500

    code merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,30 +2,12 @@ import { useEffect, useState } from "react";
 import { Button, Typography } from "antd";
 import { fetchMessage } from "../services/api";
 import Master from "./core/Master";
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignupPage from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import AurthenticateEmail from "./pages/AurthenticateEmail";
-import PassReset from "./pages/PassReset";
-import PassResetDone from "./pages/PassResetDone";
 
 const { Title, Paragraph } = Typography;
 function App() {
   return <Master />;
 }
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignupPage />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/password-reset" element={<PassReset />} />
-        <Route path="/password-reset-done" element={<PassResetDone />} />
-      </Routes>
-    </Router>
-  );
-};
-
 export default App;
+
+
+
